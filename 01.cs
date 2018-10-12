4ced276310ff7dd1652ea513f55b130a9389a1a1
jash-git <jash.liao@gmail.com>
2018-10-12 10:37:58 +0800

    Create

---
+++ 01.cs
@@ -0,0 +1,40 @@
+﻿        private void butSub020000_04_Click(object sender, EventArgs e)//新增 群組
+        {
+            if (txtSub020000_01.Text.Length > 0)
+            {
+                labSub020000_02.ForeColor = Color.Black;
+
+                int Ans = AuthorizationGroupUI2DB();
+                if (Ans == 0)
+                {
+                    initdgvSub020000_01();
+
+                    initLeftSub020000UI();
+                    m_intDB2LeftSub020000_id = -1;
+                    LeftSub020000UImode();
+
+                    initdgvSub0200_01();
+                    Leave_function();
+                }
+                else
+                {
+                    switch (Ans)
+                    {
+                        case 1:
+                            MessageBox.Show(Language.m_StrAuthorizationMsg01, Language.m_StrAuthorizationMsg00, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            break;
+                        case 2:
+                            MessageBox.Show(Language.m_StrAuthorizationMsg02, Language.m_StrAuthorizationMsg00, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            break;
+                        case 3:
+                            MessageBox.Show(Language.m_StrAuthorizationMsg03, Language.m_StrAuthorizationMsg00, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                labSub020000_02.ForeColor = Color.Red;
+                MessageBox.Show(Language.m_StrbutSub020000_04Msg01, butSub020000_04.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
---
+++ Readme.txt
@@ -0,0 +1,10 @@
+﻿比對程式碼(文字檔)差異免安裝TOOL[TextDiff、WinMerge]介紹收藏
+
+GITHUB:https://github.com/jash-git/Text-Diff-Tool
+
+軟體:
+	TextDiff(輕量化) - http://www.angusj.com/delphi/textdiff.html
+	WinMerge(功能強大) - http://winmerge.org/
+測試資料
+	01.cs
+	02.cs
